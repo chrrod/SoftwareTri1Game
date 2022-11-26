3ffde0b96eb1f32334c8e3dcd1c25ec2c89da430
Christopher Rodriguez <ckrod16@gmail.com>
2022-11-26 17:22:30 -0500

    Added pngs for the map and bloon, and created a scriptable object for bloons.

---
+++ Assets/Scripts/Bloon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBloon", menuName = "Bloon")]
+public class Bloon : ScriptableObject
+{
+    public int health;
+    public int damage;
+    public Sprite bloon;
+}
---
+++ Assets/Scripts/BloonDisplay.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BloonDisplay : MonoBehaviour
+{
+
+    public Bloon bloon;
+    public Image type;
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = bloon.bloon;
+        gameObject.layer = 0;
+    }
+}
