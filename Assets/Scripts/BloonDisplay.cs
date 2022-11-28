33ff60454df11a9b956a0c7c5d13cc5edd275d8b
Christopher Rodriguez <ckrod16@gmail.com>
2022-11-28 12:24:38 -0500

    Added bloon path

--- Assets/Scripts/BloonDisplay.cs
+++ Assets/Scripts/BloonDisplay.cs
@@ -8,12 +8,36 @@ public class BloonDisplay : MonoBehaviour
 
     public Bloon bloon;
     public SpriteRenderer spriteRenderer;
+    public float speed;
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = bloon.bloon;
-        gameObject.layer = 0;
+        gameObject.layer = 1;
+        speed = 5.0f;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "down")
+        {
+            rb.velocity = new Vector2(0, -speed);
+        }
+        else if (collision.gameObject.tag == "up")
+        {
+            rb.velocity = new Vector2(0, speed);
+        }
+        else if (collision.gameObject.tag == "right")
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else if (collision.gameObject.tag == "left")
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
     }
 }
